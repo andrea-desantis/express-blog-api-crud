730fa887b124e758eb2ce943867be7fac0de74d3
andrea-desantis <andrea.desantis290@gmail.com>
2025-12-18 22:34:11 +0100

    fix: show

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,45 +1,51 @@
 import dino from "../dino.js";
 // ----------INDEX----------
-function index(req, res){
+function index(req, res) {
 
-    const dinoIndex = dino.map(({ id, title}) => ({ id, title}));
+    const dinoIndex = dino.map(({ id, title }) => ({ id, title }));
 
     res.send(dinoIndex);
 }
 
 // ----------SHOW----------
-function show(req, res){
+function show(req, res) {
     const id = parseInt(req.params.id);
     const dinosauro = dino.find((dino) => dino.id === id);
 
-    res.json({
-        messaggio: `Dettagli del post${id}`,
-        dettagli: dinosauro
-    });
+
+    if (dinosauro !== undefined) {
+        res.json(dinosauro);
+    } else{
+        res.status(404);
+        res.json({
+            error:"Not Found",
+            message: "Dinosauro non trovato"
+        })
+    }
 }
 
 // ----------STORE----------
-function store(req, res){
+function store(req, res) {
     res.send("Creazione nuovo post")
 }
 
 // ----------UPDATE----------
-function update(req, res){
+function update(req, res) {
     const id = parseInt(req.params.id);
-        res.send(`Modifica il post ${id}`)
+    res.send(`Modifica il post ${id}`)
 }
 
 // ----------MODIFY----------
-function modify(req, res){
-    
+function modify(req, res) {
+
     const id = parseInt(req.params.id);
     res.send(`Modifica parzialmente alcuni dati del post ${id}`)
 }
 
 // ----------DESTROY----------
-function destroy(req, res){
-     const id = parseInt(req.params.id);
-        res.send(`Elimina il post ${id}`)
+function destroy(req, res) {
+    const id = parseInt(req.params.id);
+    res.send(`Elimina il post ${id}`)
 }
 
 const controller = {
